509961a7a1a8bf64f730e4f1bc4ed7c45953c940
Graziele Adeisa Monteiro De Castro <00612785130@edu.senac.br>
2021-10-20 11:58:22 -0300

    Adicionar arquivos de projeto.

---
+++ Cadastro01/Cadastro01/MainPage.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Cadastro01
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void swt(object sender, ToggledEventArgs e)
+        {
+            {
+                if (SwitchCelular.On)
+                {
+                    TxtCelular.IsEnabled = true;
+                }
+                else
+                {
+                    TxtCelular.Text = null;
+                    TxtCelular.IsEnabled = false;
+                }
+            }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            {
+                //lbNome.Text += "\n" + TxtNome.Text;
+                if (TxtNome.Text != null)
+                {
+                    lbNome.Text += "\n" + "Nome: " + TxtNome.Text;
+                }
+                else
+                {
+                    DisplayAlert("Atenção", "Preencha o campo Nome", "Ok");
+                }
+
+                if (TxtEmail.Text != null)
+                {
+                    lbNome.Text += "\n" + "E-mail: " + TxtEmail.Text;
+                }
+                else
+                {
+                    DisplayAlert("Atenção", "Preencha o campo E-mail", "Ok");
+                }
+                if (SwitchCelular.On)
+                {
+                    if (TxtCelular.Text == null)
+                    {
+                        DisplayAlert("Atenção", "Preencha o nº de telefone para contato", "Ok");
+                    }
+                    else
+                    {
+                        lbNome.Text += "\n" + "Celular: " + TxtCelular.Text;
+                    }
+
+
+                }
+
+                if (pck1.SelectedItem == null)
+                {
+                    DisplayAlert("Atenção", "Escolha uma profissão", "Ok");
+                }
+                else
+                {
+                    lbNome.Text += "\n" + "Profissão: " + pck1.SelectedItem;
+                }
+
+            }
+        }
+    }
+}
